d8b907286e13aee6f5fa909e3e3870040e9026cc
Baramee Boonsawat <richbabyface@hotmail.com>
2022-09-07 11:07:47 +0700

    new commit

--- App.js
+++ App.js
@@ -1,63 +1,96 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
+import * as React from "react";
+import { Button, View, Text, Image, StyleSheet ,SafeAreaView} from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen";
 
-const Stack = createNativeStackNavigator();
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+  },
+};
 
-function HomeScreen({ navigation, route }) {
-  React.useEffect(() => {
-    if (route.params?.post) {
-    }
-  }, [route.params?.post]);
-
+function Feed({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        title="Create Post"
-        onPress={() => navigation.navigate("CreatePost")}
-      />
-      <Text style={{ margin: 10 }}>Post:{route.params?.post}</Text>
+      {/* <Button
+        onPress={() => navigation.navigate("Notifications")}
+        title="Go to notifications"
+      /> */}
+      <Text>Feed Screen</Text>
+      <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+      <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
     </View>
   );
 }
 
-function CreatePostScreen({ navigation, route }) {
-  const [postText, setPostText] = React.useState("");
+function Article({ navigation }) {
   return (
-    <>
-      <TextInput
-        multiline
-        placeholder="Please Text here"
-        style={{ height: 200, padding: 10, backgroundColor: "white" }}
-        onChangeText={setPostText}
-        value={postText}
-      />
-      <Button
-        title="Done"
-        onPress={() => {
-          navigation.navigate("Home", { post: postText });
-        }}
-      />
-    </>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {<Button onPress={() => navigation.goBack()} title="Go back home" />}
+      <Text>Notification Screen</Text>
+    </View>
+  );
+}
+
+function CustomDrawerContent(props) {
+  return (
+<SafeAreaView style={{flex:1}}> 
+    <Image  source={require('C:/Windows/System32/navagationProject/assets/react_logo.png')}
+    style={styles.sideMenuProfileIcon}/>
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props}/>
+      <DrawerItem label="help" onPress={()=>alert('Link to help')}/>
+      {/* <DrawerItem label="ToggleDrawer" onPress={()=>props.navigation.toggleDrawer()}/> */}
+      <DrawerItem label="CloseDrawer" onPress={()=>props.navigation.closeDrawer()}/>
+
+    </DrawerContentScrollView>
+    </SafeAreaView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+  
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "white",
+          Width: 240,
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Notification" component={Article} />
+    </Drawer.Navigator>
   );
 }
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: { backgroundColor: "#008b8b" },
-          headerTintColor: "#ffff",
-          headerTitleStyle: { fontWeight: "bold", fontSize: 30 },
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
-      </Stack.Navigator>
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer />
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: "center",
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: "center",
+  },
+});
