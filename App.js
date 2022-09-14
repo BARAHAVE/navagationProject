835c9561bdf8f2e36e36f8bd7fb1afbf1948484e
Baramee Boonsawat <richbabyface@hotmail.com>
2022-09-14 10:20:31 +0700

    new com

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as React from "react";
+import { Button, View, Text, Image, StyleSheet ,SafeAreaView} from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+  },
+};
+
+
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-      <Button
-        title="Go to setting"
-        onPress={() => navigation.navigate("Setting")}
-      />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Ionicons name="home" size={30} color="#008b8b" />
+      <Text>Home Screen</Text>
     </View>
   );
 }
@@ -23,23 +38,76 @@ function SettingScreen({ navigation }) {
   );
 }
 
+
+
+
 const Tab = createBottomTabNavigator();
 
 function MyTab() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Setting" component={SettingScreen} />
-    </Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color= {color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          headerRight: () => (
+            <Ionicons name='person-add' size={25} color= {'black'} />
+          )
+
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingScreen} />
+      </Tab.Navigator>
+      
+
   );
 }
 
-const App = () => {
+
+
+
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
   return (
-    <NavigationContainer>
-      <MyTab />
+  
+    <Drawer.Navigator
+      useLegacyImplementation
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "white",
+          Width: 240,
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={ MyTab} />
+      <Drawer.Screen name="Settings" component={SettingScreen} />
+    </Drawer.Navigator>
+  );
+
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer theme={MyTheme}>
+        <MyDrawer/>
     </NavigationContainer>
   );
-};
+}
 
-export default App
+
